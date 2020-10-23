a9a452f4218dc07848afb0b07b59a58d20d5fc6f
reng <reng412@gmail.com>
2020-10-22 22:36:16 -0700

    Fix jquery for POST request

--- public/assets/js/burger_jquery.js
+++ public/assets/js/burger_jquery.js
@@ -1,29 +1,29 @@
-// AJAX call when user clicks "Add Burger"
-$(".create-form").on("submit", function (event) {
-    event.preventDefault();
-    console.log("submit button clicked");
-
-    var newBurger = {
-        name: $("#newBurger").val().trim(),
-    };
-
-    console.log(newBurger);
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-    }).then(
-        function () {
-            console.log("Added new burger");
-            // Reload the page to get the updated list
-            location.reload();
-        }
-    );
-});
-
-// AJAX call for when user clicks to "Eat"
 $(function () {
+    // AJAX call when user clicks "Add Burger"
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+        console.log("submit button clicked");
+
+        var newBurger = {
+            name: $("#newBurger").val().trim(),
+        };
+
+        console.log(newBurger);
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("Added new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    // AJAX call for when user clicks to "Eat"
     $(".eat-btn").on("click", function (event) {
         var id = $(this).data("id");
 
